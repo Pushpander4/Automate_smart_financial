940f14d2ac4ea3f68ca861392786f14b235442b3
Pushpander4 <pushpander@buzz4health.com>
2022-04-13 11:38:09 +0530

    some for tests are add in test_forgot_password.py, test_login.py, test_login_with_invalid_senarios.py and test_Signup_with_invalid _senario.py1

--- tests/automated/funtion.py
+++ tests/automated/funtion.py
@@ -4,13 +4,14 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 import time
 
-global driver
+
 
 # driver = webdriver.Chrome(ChromeDriverManager().install())
 driver=webdriver.Chrome(executable_path="C:\\chromedriver.exe")
 # parent_handel = driver.current_window_handle
 
 def open_browser():
+    global driver
     driver.maximize_window()
     driver.get("https://xenodochial-stonebraker-d239bd.netlify.app/login")
     time.sleep(3)
